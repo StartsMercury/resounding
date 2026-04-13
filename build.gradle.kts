4e77e069469f01f23635bf2b2442ac284cb26422
StartsMercury <89975834+StartsMercury@users.noreply.github.com>
2026-04-13 22:22:25 +0800

    Update buildscript to Kotlin DSL

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     // https://maven.fabricmc.net/fabric-loom/fabric-loom.gradle.plugin/
     alias(libs.plugins.fabric.loom)
-    id 'maven-publish'
+    `maven-publish`
 }
 
 base {
@@ -12,9 +12,9 @@ base {
 
 repositories {
     mavenCentral()
-    maven { url "https://maven.shedaniel.me/" }
-    maven { url "https://maven.terraformersmc.com/releases" }
-    maven { url "https://maven.maxhenkel.de/releases" }
+    maven { url = uri("https://maven.shedaniel.me/") }
+    maven { url = uri("https://maven.terraformersmc.com/releases") }
+    maven { url = uri("https://maven.maxhenkel.de/releases") }
 }
 
 java {
@@ -27,49 +27,49 @@ java {
 
 dependencies {
     // to change the versions see the gradle.properties file
-    minecraft libs.minecraft
+    minecraft(libs.minecraft)
     mappings(variantOf(libs.fabric.yarn) { classifier("v2") })
-    modImplementation libs.fabric.loader
+    modImplementation(libs.fabric.loader)
 
     // Fabric API. This is technically optional, but you probably want it anyway.
-    modImplementation libs.fabric.api
+    modImplementation(libs.fabric.api)
 
     // Cloth Config (including Auto Config)
     modApi(libs.cloth.config) {
-        exclude(group: "net.fabricmc", module: "fabric-loader")
-        exclude(group: "net.fabricmc.fabric-api")
+        exclude(group = "net.fabricmc", module = "fabric-loader")
+        exclude(group = "net.fabricmc.fabric-api")
     }
 
     modImplementation(libs.voicechat.api) {
-        exclude(group: "net.fabricmc", module: "fabric-loader")
-        exclude(group: "net.fabricmc.fabric-api")
+        exclude(group = "net.fabricmc", module = "fabric-loader")
+        exclude(group = "net.fabricmc.fabric-api")
     }
 
     modImplementation(libs.modmenu) {
-        exclude(group: "net.fabricmc", module: "fabric-loader")
-        exclude(group: "net.fabricmc.fabric-api")
+        exclude(group = "net.fabricmc", module = "fabric-loader")
+        exclude(group = "net.fabricmc.fabric-api")
     }
     // PSA: Some older mods, compiled on Loom 0.2.1, might have outdated Maven POMs.
     // You may need to force-disable transitiveness on them.
 }
 
-processResources {
-    inputs.property "version", project.version
+tasks.withType<ProcessResources> {
+    inputs.property("version", project.version)
 
     filesMatching("fabric.mod.json") {
-        expand "version": project.version
+        expand("version" to project.version)
     }
 }
 
 
-tasks.withType(JavaCompile).configureEach {
+tasks.withType<JavaCompile> {
     // ensure that the encoding is set to UTF-8, no matter what the system default is
     // this fixes some edge cases with special characters not displaying correctly
     // see http://yodaconditions.net/blog/fix-for-java-file-encoding-problems-with-gradle.html
     // If Javadoc is generated, this must be specified in that task too.
-    it.options.encoding = "UTF-8"
+    options.encoding = "UTF-8"
 }
 
-jar {
-    from "LICENSE"
+tasks.jar {
+    from("LICENSE")
 }
